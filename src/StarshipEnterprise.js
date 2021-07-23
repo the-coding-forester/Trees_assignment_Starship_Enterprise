b5485a7258e2a6574bc211ccc7081fa885f7d6a6
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-22 22:02:15 -0400

    inserting method assignOfficer complete

--- src/StarshipEnterprise.js
+++ src/StarshipEnterprise.js
@@ -10,21 +10,70 @@ class StarshipEnterprise {
   }
 
   assignOfficer(officerId, officerName) {
-    // your solution here
+    /*
+    This method should accept an officer id and name as its arguments and assign
+    a new officer to an existing officer based on experience level (as indicated by
+    the officer's id). If there's no existing officer to assign the new officer to,
+    then the new officer is the captain (e.g., Captain Picard)
+    */
+
+    if (!this.officerId || !this.officerName) {
+      // If the tree is empty then the officer is inserted as the root node
+      this.officerId = officerId;
+      this.officerName = officerName;
+    }
+
+    // If the officerId is less than the node's id, then the new node needs to live in the left-hand branch.
+    else if (officerId < this.officerId) {
+      // If node has no left-child then insert new node as left child
+      if (!this.leftReport) {
+        this.leftReport = new StarshipEnterprise(officerId, officerName, this);
+      }
+      // If node has a left-child, then recursively call the assignOfficer() method
+      else {
+        this.leftReport.assignOfficer(officerId, officerName);
+      }
+    }
+
+    // If the officerId is greater than the node's id, then the new node needs to live in the right-side branch.
+    else {
+      if (!this.rightReport) {
+        // If node has no right-child then insert new node as right child
+        this.rightReport = new StarshipEnterprise(officerId, officerName, this);
+      }
+      else {
+        // If node has a right-child, then recursively call the assignOfficer() method
+        this.rightReport.assignOfficer(officerId, officerName);
+      }
+    }
+
   }
 
   findOfficersWithNoDirectReports(values = []) {
-    // your solution here
+    /*
+    This method should return an array of officers who do not have direct reports.
+    For example: [ "Lieutenant Security-Officer", "Lt. Cmdr. LaForge", "Lieutenant Selar" ]
+    */
+
     return values;
   }
 
   listOfficersByExperience(officerNames = []) {
-    // your solution here
+    /*
+    This method should return an array of officers in increasing order of military experience.
+    For example: [ "Lieutenant Selar", "Lt. Cmdr. Crusher", "Commander Data", "Captain Picard",
+    "Lt. Cmdr. LaForge", "Commander Riker", "Lieutenant Security-Officer", "Lt. Cmdr. Worf", ]
+    */
     return officerNames;
   }
 
   listOfficersByRank(tree, rankedOfficers = {}) {
-    // your solution here
+    /*
+    This method should take the tree of commanding officers above and outline the ranking
+    officers in their ranking order.
+    Your method should return an object where each property represents the numerical rank and
+    each value consists of an array of officers (in any experience order) of a particular rank.
+    */
     return rankedOfficers;
   }
 }
